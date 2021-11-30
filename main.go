72d4c2ee496000811492616136c63facb34f0509
nsym_coding <nsym.coding@outlook.com>
2021-11-30 17:15:38 +0000

    Created the web page that accepts a string and prints it on the correct web page, now need to figure out how to convert the normal string to ascii before sending it to the /ascii-art web page

--- main.go
+++ main.go
@@ -1,4 +1,47 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
+/*This var is a pointer towards template.Template that is a
+pointer to help process the html.*/
+var tpl *template.Template
+
+/*This init function, once it's initialised, makes it so that each html file
+in the templates folder is parsed i.e. they all get looked through once and
+then stored in the memory ready to go when needed*/
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*gohtml"))
+}
+
 func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/ascii-art", asciiart)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func asciiart(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	userBanner := r.FormValue("uBanner")
+	userString := r.FormValue("uString")
+
+	d := struct {
+		Banner string
+		String string
+	}{
+		Banner: userBanner,
+		String: userString,
+	}
+
+	tpl.ExecuteTemplate(w, "ascii-art.gohtml", d)
 }
